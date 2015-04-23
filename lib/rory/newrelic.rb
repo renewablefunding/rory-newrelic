e6aadd6b0448659e87d63ba812e5093919b92786
Chuck Lauer Vose <vosechu@gmail.com>
2015-04-23 15:52:18 -0700

    initial commit

---
+++ lib/rory/newrelic.rb
@@ -0,0 +1,7 @@
+require "rory/newrelic/version"
+
+module Rory
+  module Newrelic
+    # Your code goes here...
+  end
+end
---
+++ spec/rory/newrelic_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rory::Newrelic do
+  it 'has a version number' do
+    expect(Rory::Newrelic::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
