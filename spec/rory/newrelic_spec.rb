7ee90b30a853c527e72b4682cca24a7fd6480285
Michael Irey <michael.irey@gmail.com>
2015-11-23 11:04:35 -0800

    Removes some commented code

--- spec/rory/newrelic_spec.rb
+++ spec/rory/newrelic_spec.rb
@@ -4,7 +4,6 @@ describe Rory::NewRelic do
   class TestApp < Rory::Application
     Rory::NewRelic.configure do |c|
       c.application = Rory::Application.instance
-      # c.controllers_folder = "#{Rory.root}/controllers"
       c.environments = %w(test)
     end
   end
@@ -14,8 +13,4 @@ describe Rory::NewRelic do
   end
 
   subject { described_class.instance }
-
-  # it 'uses sane defaults from Rory' do
-  #   expect(subject).to receive(:controllers_folder)
-  # end
 end
